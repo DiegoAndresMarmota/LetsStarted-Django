8c1d23d7d79d843a0a67e443a4421087b71cfb55
Diego Andres Echalar <99225490+DiegoAndresMarmota@users.noreply.github.com>
2023-01-11 21:45:22 -0300

    Login y vistas de tareas

--- tasks/views.py
+++ tasks/views.py
@@ -1,9 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from django.http import HttpResponse
+from django.contrib.auth import login
+from django.db import IntegrityError
+
+# from django.views.decorators.csrf import csrf_protect
 
 # Create your views here.
+# @csrf_protect
 
 def home(request):
     return render(request, 'home.html')
@@ -20,8 +24,9 @@ def signup(request):
             try:
                 user = User.objects.create_user(username=request.POST['username'], password = request.POST['password1'])
                 user.save()
-                return HttpResponse('Usuario creado satisfactoriamente')
-            except:
+                login(request, user)
+                return redirect('tasks')
+            except IntegrityError:
                 return render(request, 'signup.html', {
                     'form': UserCreationForm,
                     "error": "Usuario ya existe"
@@ -31,3 +36,5 @@ def signup(request):
             "error": "Contraseña no coincide"
         })
         
+def tasks(request):
+    return render(request, 'tasks.html')
